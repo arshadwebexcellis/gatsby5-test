1018acbfa2ca31fdcf8ed004d8700d8a85c5864e
ankurwebexcellis <64285081+arshadwebexcellis@users.noreply.github.com>
2023-05-31 18:30:49 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby5-demo`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
